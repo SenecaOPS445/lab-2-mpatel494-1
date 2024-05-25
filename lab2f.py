782f02e36578f9638541dff44caf1e29f10e119a
Manav Patel <mpatel494@myseneca.ca>
2024-05-25 01:32:37 -0400

    Lab2 Done (forgot to edit dates and name)!

--- lab2f.py
+++ lab2f.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #Author: Manav Patel
 #Author ID: mpatel494
-#Date_Created:  2024/01/29
+#Date_Created:  25 May 2024
 import sys
 timer = int(sys.argv[1]) 
 while timer != 0:
--- lab2g.py
+++ lab2g.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #Author: Manav Patel
 #Author ID: mpatel494
-#Date_Created:  2024/01/29
+#Date_Created: 25 May 2024
 import sys
 timer = 3 
 if len(sys.argv) == 2:
